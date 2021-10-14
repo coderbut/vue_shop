9aa27547b4fac41f540bca2e3b309798c49423ec
贺雨杭 <1186891236@qq.com>
2021-10-14 23:50:06 +0800

    完成登录页面

--- src/main.js
+++ src/main.js
@@ -1,11 +1,19 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import './plugins/element.js'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import "./plugins/element.js";
+import "./assets/css/global.css";
+// 导入字体图标
+import "./assets/fonts/iconfont.css";
+// 导入axios
+import axios from "axios";
+axios.defaults.baseURL = "http://127.0.0.1:8888/api/private/v1/";
 
-Vue.config.productionTip = false
+Vue.prototype.$http = axios;
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
